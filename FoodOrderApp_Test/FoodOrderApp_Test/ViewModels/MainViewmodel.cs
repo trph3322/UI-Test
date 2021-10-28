d45b1cffebb9617a6278ca9b1048d917a3d4ed68
Phuc Tran <tr.ph3322@gmail.com>
2021-10-28 13:56:18 +0700

    update new pj

---
+++ FoodOrderApp_Test/FoodOrderApp_Test/ViewModels/MainViewmodel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodOrderApp_Test.Views.UserControls;
+
+namespace FoodOrderApp_Test.ViewModels
+{
+     
+    public class MainViewmodel : BaseViewModel
+    {
+        public ControlBarUC cb;
+        
+        public MainViewmodel()
+        {
+            cb = new ControlBarUC();
+            cb.centerBar.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
