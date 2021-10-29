a82ee189efb4afca4f3de5a2ec7f40ea3d935ff5
Phuc Tran <tr.ph3322@gmail.com>
2021-10-29 15:37:29 +0700

    Fix some bugs and complete control bar.

---
+++ FoodOrderApp_Test/FoodOrderApp_Test/ViewModels/ControlBarViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FoodOrderApp_Test.ViewModels
+{ 
+    public class ControlBarViewModel : BaseViewModel
+    {
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => p == null ? false : true, p => {
+                FrameworkElement window = GetParentWindow(p);
+                var w = window as Window;
+                
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => p == null ? false : true, p => {
+                FrameworkElement window = GetParentWindow(p);
+                var w = window as Window;
+
+                if (w != null)
+                {
+                    w.WindowState = WindowState.Minimized;
+                }
+            });
+            MouseMoveWindowCommand = new RelayCommand<UserControl>((p) => p == null ? false : true, p => {
+                FrameworkElement window = GetParentWindow(p);
+                var w = window as Window;
+
+                if (w != null)
+                {
+                    w.DragMove();
+                }
+            });
+        }
+
+        FrameworkElement GetParentWindow(UserControl p)
+        {
+            FrameworkElement res = p;
+            while(res.Parent != null)
+            {
+                res = res.Parent as FrameworkElement;
+            }
+            return res;
+        } 
+    }
+}
--- FoodOrderApp_Test/FoodOrderApp_Test/Views/UserControls/ControlBarUC.xaml.cs
+++ FoodOrderApp_Test/FoodOrderApp_Test/Views/UserControls/ControlBarUC.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodOrderApp_Test.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace FoodOrderApp_Test.Views.UserControls
     /// </summary>
     public partial class ControlBarUC : UserControl
     {
+        public ControlBarViewModel viewModel { get; set; }
         public ControlBarUC()
         {
             InitializeComponent();
+            this.DataContext = viewModel = new ControlBarViewModel();
         }
     }
 }
