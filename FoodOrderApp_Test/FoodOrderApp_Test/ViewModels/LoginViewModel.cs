68716491e0c15f994eb8a5cc5da8d26716c47505
Phuc Tran <tr.ph3322@gmail.com>
2021-11-01 07:40:45 +0700

    add event click

--- FoodOrderApp_Test/FoodOrderApp_Test/ViewModels/LoginViewModel.cs
+++ FoodOrderApp_Test/FoodOrderApp_Test/ViewModels/LoginViewModel.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using FoodOrderApp_Test.Views;
 
 namespace FoodOrderApp_Test.ViewModels
 {
     class LoginViewModel : BaseViewModel
     {
+        public ICommand OpenForgotPasswordWDCommand { get; set; }
+        public ICommand OpenSignUpWDCommand { get; set; }
         public LoginViewModel()
         {
+            OpenForgotPasswordWDCommand = new RelayCommand<LoginWindow>((parameter) => true, (parameter) => OpenForgotPasswordWindow(parameter));
+            OpenSignUpWDCommand = new RelayCommand<LoginWindow>((parameter) => true, (parameter) => OpenSignUpWindow(parameter));
         }
 
-    }
+
+        public void OpenForgotPasswordWindow(LoginWindow parameter)
+        {
+            DoubleAnimation fadeIn = new DoubleAnimation(0.5, 1.0, new Duration(TimeSpan.FromSeconds(0.3)));
+
+            ForgotPasswordWindow forgotPasswordWindow = new ForgotPasswordWindow();
+            forgotPasswordWindow.BeginAnimation(Window.OpacityProperty, fadeIn);
+            forgotPasswordWindow.ShowDialog();
+        }
+
+        public void OpenSignUpWindow(LoginWindow parameter)
+        {
+            DoubleAnimation fadeIn = new DoubleAnimation(0.5, 1.0, new Duration(TimeSpan.FromSeconds(0.3)));
+
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.BeginAnimation(Window.OpacityProperty, fadeIn);
+            signUpWindow.ShowDialog();
+        }
+    } 
 }
